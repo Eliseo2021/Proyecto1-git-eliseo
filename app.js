e21ac10a7e028093db3e8aa50e2d2bfaf807e227
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2021-11-16 10:35:12 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const countries = require('./countries.json');
+
+/** Get all the data of one single country given the ISO3 code.
+ * @param  {string} isoCode
+ * @returns {Object}
+ */
+function getCountryByIso3(isoCode){
+    //Complete with your code
+    return countries.find(country => country.iso3.includes(isoCode))
+}
+
+/**
+ * Get a country translated name given his ISO3 code and tranlated language.
+ * @param  {string} isoCode
+ * @param  {string} language example: "es"
+ * @returns {string}
+ */
+function getCountryTranslatedName(isoCode, language){
+    //Complete with your code
+    const country = countries.find(name => name.iso3 === isoCode)
+    for(const key in country.translations){
+       if( key ===language){
+           return country.translations[key]
+        }
+    }
+}
+/**Get an array of all the countries with the specified subregion
+ * @param  {string} subregion
+ * @returns {Array}
+ */
+function getCountriesBySubregion(subregion){
+    //Complete with your code
+    return countries.filter(region => region.subregion.includes(subregion))
+}
+
+function main() {
+    console.log(
+        '///// Ejercicio 1 /////\n',
+        getCountryByIso3('ARG'),
+        '\n');
+    console.log(
+        '///// Ejercicio 2 /////\n',
+        getCountryTranslatedName('ARG', 'ja'),
+        '\n');
+    console.log(
+        '///// Ejercicio 3 /////\n',
+        getCountriesBySubregion('South America'),
+        '\n');
+
+}
+
+main();
